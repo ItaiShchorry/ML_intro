6739fa706e4c7b15c212c324272a2e887af1c69e
Avi <avi.c33@gmail.com>
2016-11-20 19:34:48 +0200

    Added graphs and prints for Q1

--- main.py
+++ main.py
@@ -16,23 +16,45 @@ def main():
     train_labels = labels[idx[:10000]]
     test = data[idx[10000:], :]
     test_labels = labels[idx[10000:]]
-    print("banko")
     # section B
     k = 10
     precision = runKnnAndReturnPrecision(train[:1000], train_labels[:1000], test, test_labels, k)
+    print('Precision for section B:', precision)
 
+    # Creating all pairs of distances of the training pictures with the test pictures
+    # It will save running time for sections C and D.
     pairs = np.array([np.array([(LA.norm(np.array(test[j]) - np.array(train[i])), train_labels[i])\
                       for i in range(5000)]) for j in range(test.shape[0])])
+
+    # Plotting configurations for sections C and D.
+    ax0 = plt.subplot(211)
+    ax0.set_xlabel('k')
+    ax0.set_ylabel('precision')
+    plt.text(0.5, 0.5, 'Section C',
+         horizontalalignment='center',
+         verticalalignment='center',
+         transform=ax0.transAxes)
+    ax1 = plt.subplot(212)
+    ax1.set_ylabel('precision')
+    ax1.set_xlabel('training samples')
+    plt.text(0.5, 0.5, 'Section D',
+             horizontalalignment='center',
+             verticalalignment='center',
+             transform=ax1.transAxes)
 
     # section C
     precisions = np.array([])
     ks_array = np.array([k for k in range(1, 101)])
+    max = 0
+    max_k = 0
     for k in ks_array:
         precision = secCandD(pairs, test_labels, k, 1000)
         precisions = np.append(precisions, precision)
-    plt.axis([1, 100, 0, 1])
-    plt.plot(ks_array, precisions, 'ro')
-    plt.show()
+        if precision > max:
+            max_k = k
+            max = precision
+    print('K = {} gives the maximum precision in section C.'.format(max_k))
+    ax0.plot(ks_array, precisions)
 
     # section D
     training_samples = [x for x in range(100, 5100, 100)]
@@ -41,8 +63,7 @@ def main():
     for n in training_samples:
         precision = secCandD(pairs, test_labels, k, n)
         precisions = np.append(precisions, precision)
-    plt.axis([100, 5000, 0, 1])
-    plt.plot(training_samples, precisions, 'ro')
+    ax1.plot(training_samples, precisions)
     plt.show()
 
 def runKnnAndReturnPrecision(train, train_labels, test , test_labels, k):
